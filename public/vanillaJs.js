c4dd384c51d9629e166f1cb5197907e54e06e507
Övgün Taş <ovguntas@icloud.com>
2022-05-13 22:40:16 +0300

    display poetry added

--- public/vanillaJs.js
+++ public/vanillaJs.js
@@ -10,13 +10,16 @@ showHideBtn.addEventListener("click", () => {
     if (time.classList.contains("noclicked")) {
         time.classList.toggle("noclicked");
         // console.log("noclicked removed");
-        time.hidden = true
-
+        time.hidden = true;
+        document.querySelector("#poetry").classList.toggle("d-none")
+        document.querySelector("#poetry").style.fontSize = "1rem"
 
     } else {
         time.classList.toggle("noclicked");
         // console.log("noclicked added")
-        time.hidden = false
+        time.hidden = false;
+        document.querySelector("#poetry").classList.toggle("d-none");
+
 
     }
 })
